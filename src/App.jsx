11f34bcc58c59e62cdcba3c1c0fa5c802bb7774a
abdullahanwar21 <abdullahmeer396@gmail.com>
2024-01-05 23:30:47 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { MdDelete } from "react-icons/md";
 import { FaCheck } from "react-icons/fa";
 import "./App.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default function App() {
   let [allTodo, setAllTodo] = useState([]);
@@ -79,101 +81,118 @@ export default function App() {
   }, []);
   return (
     <div className="App">
-      <h1 className="text-4xl mt-5">My Todo's</h1>
-      <div className="todo-wrapper flex flex-col">
-        <form>
-          <div className="todo-input">
-            <div className="todo-input-item">
-              <label htmlFor="title">Title</label>
-              <input
-                type="text"
-                placeholder="Enter Title of Your Task "
-                onChange={(e) => setNewTitle(e.target.value)}
-                value={newTitle}
-                required
-              />
+      <div className="container">
+          <h1 className="text-4xl mb-5">My <span className="text-warning">Todo's</span></h1>
+        <div className="row">
+          <div className="offset-md-1 col-md-10 offset-md-1">
+            <div className="row">
+              <div className="col-md-5 my-2">
+                <label htmlFor="title">Title</label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Title of Your Task "
+                  id="title"
+                  onChange={(e) => setNewTitle(e.target.value)}
+                  value={newTitle}
+                  required
+                />
+              </div>
+              <div className="col-md-5 mb-2">
+                <Form.Label htmlFor="Desc">Description</Form.Label>
+                <Form.Control
+                  type="text"
+                  id="Desc"
+                  placeholder="Enter Description of Your Task "
+                  onChange={(e) => setNewDesc(e.target.value)}
+                  value={newDesc}
+                />
+              </div>
+              <div className="col-md-2 ">
+                <Button
+                  type="button"
+                  className="btn btn-warning text-light p-2 mt-4 fw-bold  mb-2"
+                  onClick={handleAddTodo}
+                >
+                  Add Todo
+                </Button>
+              </div>
             </div>
-            <div className="todo-input-item">
-              <label htmlFor="title">Description</label>
-              <input
-                type="text"
-                placeholder="Enter Description of Your Task "
-                onChange={(e) => setNewDesc(e.target.value)}
-                value={newDesc}
-              />
+            <div className="container">
+              <div className="row">
+                <div className="offset-md-2 col-md-8 offset-md-2">
+            <div className="todo-wrapper">
+             
+              <div className="btn-area">
+                <button
+                  className={`secondaryBtn ${
+                    isCompleted === false && "active"
+                  }`}
+                  onClick={() => setIsCompleted(false)}
+                >
+                  Todo
+                </button>
+                <button
+                  className={`secondaryBtn ${isCompleted === true && "active"}`}
+                  onClick={() => setIsCompleted(true)}
+                >
+                  Completed
+                </button>
+              </div>
+              {isCompleted === false &&
+                allTodo.map((item, index) => {
+                  return (
+                    <div className="todo-list" key={index}>
+                      <div className="todo-list-item">
+                        <div>
+                          <h3>{item.title}</h3>
+                          <p>{item.desc}</p>
+                        </div>
+                        <div className="flex justify-evenly">
+                          <MdDelete
+                            className="icon"
+                            title="Delete?"
+                            onClick={() => deleteTodo(index)}
+                          />
+                          <FaCheck
+                            className="check-icon"
+                            onClick={() => updatedTodo(index)}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              {isCompleted === true &&
+                completedTodo.map((item, index) => {
+                  return (
+                    <div className="todo-list" key={index}>
+                      <div className="todo-list-item">
+                        <div>
+                          <h3>{item.title}</h3>
+                          <p>{item.desc}</p>
+                          <p>
+                            <small>Completed On : {item.completedOn}</small>
+                          </p>
+                        </div>
+                        <div>
+                          <MdDelete
+                            className="icon"
+                            title="Delete?"
+                            onClick={() => deleteCompleteTodo(index)}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
             </div>
-            <div className="todo-input-item">
-              <button
-                type="button"
-                className="primaryBtn"
-                onClick={handleAddTodo}
-              >
-                Add Todo
-              </button>
+
+                </div>
+              </div>
             </div>
+
           </div>
-        </form>
-        <div className="btn-area">
-          <button
-            className={`secondaryBtn ${isCompleted === false && "active"}`}
-            onClick={() => setIsCompleted(false)}
-          >
-            Todo
-          </button>
-          <button
-            className={`secondaryBtn ${isCompleted === true && "active"}`}
-            onClick={() => setIsCompleted(true)}
-          >
-            Completed
-          </button>
         </div>
-        {isCompleted === false &&
-          allTodo.map((item, index) => {
-            return (
-              <div className="todo-list" key={index}>
-                <div className="todo-list-item">
-                  <div>
-                    <h3>{item.title}</h3>
-                    <p>{item.desc}</p>
-                  </div>
-                  <div className="flex justify-evenly">
-                    <MdDelete
-                      className="icon"
-                      title="Delete?"
-                      onClick={() => deleteTodo(index)}
-                    />
-                    <FaCheck
-                      className="check-icon"
-                      onClick={() => updatedTodo(index)}
-                    />
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        {isCompleted === true &&
-          completedTodo.map((item, index) => {
-            return (
-              <div className="todo-list" key={index}>
-                <div className="todo-list-item">
-                  <div>
-                    <h3>{item.title}</h3>
-                    <p>{item.desc}</p>
-                    <p>
-                      <small>Completed On : {item.completedOn}</small>
-                    </p>
-                  </div>
-                  <div>
-                    <MdDelete
-                      className="icon"
-                      title="Delete?"
-                      onClick={() => deleteCompleteTodo(index)}
-                    />
-                  </div>
-                </div>
-              </div>
-            );
-          })}
       </div>
     </div>
   );
